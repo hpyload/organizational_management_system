b7232bca074e61b544804e87d9559f75a5d5916e
nabil <n.oumellahni@gmail.com>
2023-03-20 17:12:09 +0000

    Spring Boot - Microservices

--- employee-service/src/main/java/com/oumellahni/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/oumellahni/employeeservice/EmployeeServiceApplication.java
@@ -2,9 +2,7 @@ package com.oumellahni.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
