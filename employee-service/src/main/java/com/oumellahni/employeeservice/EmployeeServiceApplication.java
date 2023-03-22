7886e51a12af89b683cdc64cf3df5ca5947195d7
nabil <n.oumellahni@gmail.com>
2023-03-22 15:18:20 +0000

    Spring Boot - Microservices

--- employee-service/src/main/java/com/oumellahni/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/oumellahni/employeeservice/EmployeeServiceApplication.java
@@ -11,10 +11,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 // @EnableFeignClients annotation enables component scanning
 // for interfaces that declare they Feign clients
 public class EmployeeServiceApplication {
-//    @Bean
-//    public WebClient webClient() {
-//        return WebClient.builder().build();
-//    }
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
